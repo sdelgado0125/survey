1a7becd72585de9e9c7367d287ab87c0101f7fb6
sdelgado0125 <130323215+sdelgado0125@users.noreply.github.com>
2024-05-01 19:56:30 -0400

    Update app.py
    added flash, session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 from surveys import satisfaction_survey
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -16,7 +16,7 @@ def base():
 
 @app.route('/start', methods=['GET','POST'])
 def start_survey():
-    global responses
+    session['responses'] = []
     
 
     if request.method == 'POST':
@@ -25,26 +25,31 @@ def start_survey():
 
 @app.route('/questions/<int:question_number>', methods=['GET', 'POST'])
 def question(question_number):
-    global responses
 
     current_question = satisfaction_survey.questions[question_number]
 
     if request.method == 'POST':
         
         choice = request.form['answer']
+        responses = session.get('responses', [])
         responses.append(choice)
+        session['responses'] = responses
 
         next_question = question_number + 1
         if next_question < len(satisfaction_survey.questions):
             return redirect(url_for('question', question_number=next_question))
         else:
             return redirect(url_for('thank_you'))
+    
+    if question_number != len(responses):
+        flash('Please answer the questions in order.')
+        return redirect(url_for('question', question_number=len(responses)))
         
     return render_template('questions.html', question_number=question_number, current_question=current_question)
 
 @app.route('/answer', methods = ['GET','POST'])
 def thank_you():
-    global responses
+    responses = session.get('responses', [])
     return render_template("thankyou.html", responses=responses)
     
 
